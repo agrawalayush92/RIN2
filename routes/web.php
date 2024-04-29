1d07e49701a84c899dbd8d73031c56d9bac1119e
Ayush Agarwal <agrawalayush92@gmail.com>
2024-04-29 09:05:56 +0530

    routes and relation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/impersonate/{userId}', [UserController::class, 'impersonate']);
+Route::post('/notifications', [UserController::class, 'notifications']);
+Route::get('/settings', [UserController::class, 'settings']);
